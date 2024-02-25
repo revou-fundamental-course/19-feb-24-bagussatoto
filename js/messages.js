889df4330b4358d31bdcce42ee285b25fbf73cda
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2024-02-25 09:50:49 +0700

    p

---
+++ js/messages.js
@@ -0,0 +1,25 @@
+// Function Validate
+function validateForm(){
+    const name = document.forms["message-form"]["full-name"].value;
+    const birthDate = document.forms["message-form"]["birth-date"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const messages = document.forms["message-form"]["messages-user"].value;
+
+    if(name == "" || birthDate == "" || gender == "" || messages == ""){
+        alert("Form tidak boleh ada yang kosong!");
+        return false;
+    }
+    
+    setSenderUI(name, birthDate, gender, messages);
+    
+    return false;
+}
+
+function setSenderUI(name, birthDate, gender, messages){
+    var dt = new Date();
+    document.getElementById('current-time').innerText=dt;
+    document.getElementById("sender-full-name").innerText = name;
+    document.getElementById("sender-birth-date").innerText = birthDate;
+    document.getElementById("sender-gender").innerText = gender;
+    document.getElementById("sender-messages").innerText = messages;
+}
